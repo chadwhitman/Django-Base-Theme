2f9c4f5758654ce34b5b8a89d6195cb86eec7af5
Chad Whitman <chad.whitman@yahoo.com>
2014-12-09 10:24:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description="A responsive base theme for Wharton Django applications.",
     url='https://github.com/chadwhitman/Base-Theme/',
     author='Chad Whitman, the Wharton School',
-    data_files = [(str(TEMPLATE_DIRS), ['base_theme/templates/base.html']),],
+    data_files = ['/%s/%s/%s/templates', ['base_theme/templates/base.html']),],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
