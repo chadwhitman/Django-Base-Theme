db422c9e40dafb0a6835c57344e14d206ef1143a
Chad Whitman <chad.whitman@yahoo.com>
2014-12-09 09:32:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ from distutils.core import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 #data_folder = os.path.join(BASE_DIR, '/templates/')
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 setup(
     name='base_theme',
     version='1.0',
