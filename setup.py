3e5cf5b0251ef9a528b4a8e457d7a29457c6e5a2
Chad Whitman <chad.whitman@yahoo.com>
2014-12-10 15:30:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath('__file__'), os.pardir)))
 
+import base_theme
+
 setup(
     name='base_theme',
     version='1.0',
