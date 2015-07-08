e54d2d4db6d180812ad147cd043dbf34ab6a3a0b
Whitman, Chad <cwhitman@wharton.upenn.edu>
2015-07-08 11:28:21 -0400

    js

--- base_theme/static/base_theme/js/custom.js
+++ base_theme/static/base_theme/js/custom.js
@@ -3,14 +3,15 @@
 (function () {
 	$('#header__nav__search').hide();
 		$('.search__glyph__icon.header').click(function() {
-			$('.search__glyph__icon.header').slideToggle("fast", function() {			
+			$('#header__nav__search').slideToggle("fast", function() {			
 		});
   });
   
   $('#main_nav__search').hide();
 		$('.search__glyph__icon.main_nav').click(function() {
-			$('.search__glyph__icon.main_nav').slideToggle("fast", function() {			
+			$('#main_nav__search').slideToggle("fast", function() {			
 		});
   });
   
 }());
+
